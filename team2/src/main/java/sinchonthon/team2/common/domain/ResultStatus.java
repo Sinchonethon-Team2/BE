33200cd6628f18c1637d5f91c5bae96e559e347e
Jongyun Won <jywon1128@gmail.com>
2025-09-05 22:13:34 +0900

    project init

--- team2/src/main/java/sinchonthon/team2/common/domain/ResultStatus.java
+++
@@ -1,5 +0,0 @@
-package sinchonthon.team2.common.domain;
-
-public enum ResultStatus {
-    SUCCESS, FAILURE, NEUTRAL
-}
--- team2/src/main/java/sinchonthon/team2/member/domain/School.java
+++
@@ -1,5 +0,0 @@
-package sinchonthon.team2.member.domain;
-
-public enum School {
-    SOGANG, YONSEI, HONGIK, EWHA
-}
